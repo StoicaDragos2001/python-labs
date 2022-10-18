61f2a0536eb409a6a3e0275a8dce638fc2c78924
Stoica Dragos <stoicadragos2001@gmail.com>
2022-10-18 09:50:28 +0300

    part 2

--- python_lab2/main.py
+++ python_lab2/main.py
@@ -1,4 +1,5 @@
 from math import sqrt
+import numpy as np
 
 
 def exercise1(n):
@@ -96,6 +97,52 @@ def exercise8(data, x=1, flag=True):
     return solution
 
 
+def exercise9(spectators):
+    solution = []
+    for i in range(spectators.shape[1]):
+        col = spectators[::-1, i]
+        for j in range(spectators.shape[0] - 1):
+            if col[j] <= max(col[j + 1:]):
+                solution.append((spectators.shape[0] - j - 1, i))
+    return solution
+
+
+def exercise10(*argv):
+    solution = []
+    matrix = []
+    size = max([len(x) for x in argv])
+    for arg in argv:
+        arg.extend([None for _ in range(size - len(arg))])
+        matrix.append(arg)
+    matrix = np.array(matrix)
+    for i in range(matrix.shape[1]):
+        solution.append(tuple(matrix[:, i]))
+    return solution
+
+
+def sort_key(e):
+    return e[:][1][2]
+
+
+def exercise11(tuples):
+    tuples.sort(key=sort_key)
+    return tuples
+
+
+def exercise12(words):
+    rhymes = {}
+    result = []
+    for word in words:
+        last2 = word[-2:]
+        if last2 not in rhymes:
+            rhymes[last2] = [word]
+        else:
+            rhymes[last2].append(word)
+    for last2, words in rhymes.items():
+        result.append(words)
+    return result
+
+
 if __name__ == '__main__':
     print(f"exercise 1:\n{exercise1(8)}")
     print(f"exercise 2:\n{exercise2([i for i in range(12)])}")
@@ -108,3 +155,11 @@ if __name__ == '__main__':
     print(f'exercise 6:\n{exercise6([1, 2, 3], [2, 3, 4], [4, 5, 6], [4, 1, "test"], x=2)}')
     print(f"exercise 7:\n{exercise7([121, 123, 357753, 42, 11, 343, 59])}")
     print(f'exercise 8:\n{exercise8(["test", "hello", "lab002"], x=2, flag=False)}')
+    exercise9_input = np.array([[1, 2, 3, 2, 1, 1],
+                                [2, 4, 4, 3, 7, 2],
+                                [5, 5, 2, 5, 6, 4],
+                                [6, 6, 7, 6, 7, 5]])
+    print(f'exercise 9:\n{exercise9(exercise9_input)}')
+    print(f'exercise 10:\n{exercise10([1, 2, 3], [5, 6, 7], ["a", "b", "c"], [9])}')
+    print(f"exercise 11:\n{exercise11([('abc', 'bcd'), ('abc', 'zza')])}")
+    print(f"exercise 12:\n{exercise12(['ana', 'banana', 'carte', 'arme', 'parte'])}")
